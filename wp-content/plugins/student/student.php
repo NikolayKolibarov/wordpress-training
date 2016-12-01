8c43b2831e12fb4b2ba59565b698f602627a8d97
nikolaykolibarov <nikolaykolibarov@gmail.com>
2016-12-01 12:47:03 +0200

    Added Student CPT

--- wp-content/plugins/student/student.php
+++ wp-content/plugins/student/student.php
@@ -16,8 +16,9 @@ if (!function_exists('add_action')) {
 
 // Includes
 include('includes/activate.php');
+include('includes/init.php');
 
 // Hooks
 register_activation_hook(__FILE__, 'nnk_activate_plugin');
-
+add_action('init', 'student_init');
 // Shortcodes
