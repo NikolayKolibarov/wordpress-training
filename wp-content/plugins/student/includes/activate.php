4466517764eb19ae93d6e34a78506a99f02f99e5
nikolaykolibarov <nikolaykolibarov@gmail.com>
2016-12-01 12:11:40 +0200

    Added plugin activation. Added security to prevent plugin direct call.

---
+++ wp-content/plugins/student/includes/activate.php
@@ -0,0 +1,7 @@
+<?php
+
+function nnk_activate_plugin() {
+    if (version_compare(get_bloginfo('version'), '4.2', '<')) {
+        wp_die(__('You must update WordPress to use this plugin.', 'student'));
+    }
+} 
--- wp-content/plugins/student/student.php
+++ wp-content/plugins/student/student.php
@@ -6,3 +6,18 @@
  * Author: Nikolay
  * Text Domain: student
  */
+
+if (!function_exists('add_action')) {
+    echo 'Not allowed. Do not call me directly.';
+    exit();
+}
+
+// Setup
+
+// Includes
+include('includes/activate.php');
+
+// Hooks
+register_activation_hook(__FILE__, 'nnk_activate_plugin');
+
+// Shortcodes
