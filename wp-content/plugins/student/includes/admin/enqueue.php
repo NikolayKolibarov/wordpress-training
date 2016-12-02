9fbbb8fc49d576c2fa089145a57a8367ec73ea3f
nikolaykolibarov <nikolaykolibarov@gmail.com>
2016-12-02 10:36:39 +0200

    Register and enqueue jquery because it's a dependency of bootstrap

--- wp-content/plugins/student/includes/admin/enqueue.php
+++ wp-content/plugins/student/includes/admin/enqueue.php
@@ -10,6 +10,8 @@ function nnk_admin_enqueue() {
 	wp_register_style('nnk_bootstrap', plugins_url('/assets/css/bootstrap.css', STUDENT_PLUGIN_URL));
 	wp_enqueue_style('nnk_bootstrap');
 
+	wp_register_script( 'nnk_jquery', 'https://code.jquery.com/jquery-3.1.1.js' );
 	wp_register_script('nnk_bootstrap', plugins_url('/assets/js/bootstrap.js', STUDENT_PLUGIN_URL));
+	wp_enqueue_script('nnk_jquery');
 	wp_enqueue_script('nnk_bootstrap');
 }
